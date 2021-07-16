40a7b3caadea132d463dc3fc6274bfa47351f7a7
Bryan Pitman <77179060+bryanpitman@users.noreply.github.com>
2021-07-16 16:52:28 -0700

    Update script.js
    steps 1 - 7 completed and tested

--- js/script.js
+++ js/script.js
@@ -4,12 +4,47 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {
+    Quote: 'Alright everyone chill',
+    Source: 'Mr. Freeze',
+    Citation: "Batman and Robin",
+    Year: 1997
+  },
+  {
+    Quote: 'Apes together strong',
+    Source: 'Caesar',
+    Citation: 'Rise of the Planet of the Apes',
+    Year: 2011
+  },
+  {
+    Quote: 'I am Groot',
+    Source: 'Groot',
+    Citation: 'Guardians of the Galaxy',
+    Year: 2014
+  },
+  {
+    Quote: 'There is no spoon',
+    Source: 'Young Monk',
+    Citation: 'The Matrix',
+    Year: 1999
+  },
+  {
+    Quote: 'Why so serious?',
+    Source: 'Joker',
+    Citation: 'The Dark Knight',
+    Year: 2008
+  }
+]
+
+
+
 
 
 
